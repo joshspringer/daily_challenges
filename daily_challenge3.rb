253fd47eef3ad15efd08516204de40d99dcf993d
Josh <jspringxc@gmail.com>
2017-03-07 22:23:51 -0500

    refactored challenge 3 using select method

--- daily_challenge3.rb
+++ daily_challenge3.rb
@@ -1,8 +1,17 @@
 numbers = [1, 2, 4, 2]
 even_numbers = []
-numbers.each do |number|
-  if number.even?
-    even_numbers << number
-  end
-end
+# numbers.each do |number|
+#   if number.even?
+#     even_numbers << number
+#   end
+# end
+
+even_numbers = numbers.select { |num| num.even? }
+
 p even_numbers
+
+odd_numbers = []
+
+odd_numbers = numbers.select { |num| num.odd? }
+
+p odd_numbers
